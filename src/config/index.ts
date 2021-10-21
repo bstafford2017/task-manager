5fbbb4a64f229ddaf55adaec9bac6aa352d67fcc
bstafford2017 <b.stafford2017@gmail.com>
2021-10-21 18:50:48 -0500

    Convert project to web app

--- src/config/index.ts
+++
@@ -1,1 +0,0 @@
-export const secret = 'my-temporary-secret-which-will-be-put-in-secrets-manager'
--- src/interfaces/task.ts
+++
@@ -1,8 +0,0 @@
-export default interface Task {
-  id: string
-  title: string
-  category: string
-  description: string
-  important: boolean
-  date: number
-}
